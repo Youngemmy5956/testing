98aa9eb5ca15e1d9d63a096155ab77d78040cf4d
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2025-02-22 07:09:34 +0100

    updated

--- api/routes/user-routes.js
+++ api/routes/user-routes.js
@@ -1,9 +1,16 @@
+// filepath: /c:/Users/user/Desktop/testing/api/routes/auth-routes.js
+
 import express from 'express';
-import { signup, signin } from '../controllers/user-controller.js';
+import passport from 'passport';
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/signin', signin);
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/'); // Redirect to your frontend application
+  }
+);
 
 export default router;
