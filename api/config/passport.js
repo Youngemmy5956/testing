8d4cc08c2101b54b027e97e8898bc1b35699d7e1
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2025-02-22 07:08:21 +0100

    UPDATED

---
+++ api/config/passport.js
@@ -0,0 +1,42 @@
+// filepath: /c:/Users/user/Desktop/testing/api/config/passport.js
+
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import User from '../models/User.js';
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/google/callback'
+},
+async (accessToken, refreshToken, profile, done) => {
+  try {
+    let user = await User.findOne({ googleId: profile.id });
+    if (!user) {
+      user = new User({
+        googleId: profile.id,
+        username: profile.displayName,
+        email: profile.emails[0].value
+      });
+      await user.save();
+    }
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+export default passport;
