e3622c1ea8521ca4748b454ea6c9d95ac9a664f5
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2025-03-02 19:21:05 +0100

    added

--- api/routes/user-routes.js
+++ api/routes/user-routes.js
@@ -1,3 +1,5 @@
+// filepath: /c:/Users/user/Desktop/testing/api/routes/user-routes.js
+
 import express from 'express';
 import { signup, signin } from '../controllers/user-controller.js';
 import passport from 'passport';
@@ -11,6 +13,15 @@ router.post('/signin', signin);
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/'); // Redirect to your frontend application
+  }
+);
+
+// Facebook authentication routes
+router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+
+router.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
   (req, res) => {
     res.redirect('/'); // Redirect to your frontend application
   }
