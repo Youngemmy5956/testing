0222cdc95d41ba4ec04b11c45d76563a639a3a01
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2025-03-02 19:19:55 +0100

    updated

--- api/config/passport.js
+++ api/config/passport.js
@@ -1,14 +1,16 @@
+// filepath: /c:/Users/user/Desktop/testing/api/config/passport.js
 
-import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import User from "../models/User.js";
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { Strategy as FacebookStrategy } from 'passport-facebook';
+import User from '../models/User.js';
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: '/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -16,6 +18,33 @@ passport.use(
         if (!user) {
           user = new User({
             googleId: profile.id,
+            username: profile.displayName,
+            email: profile.emails[0].value,
+          });
+          await user.save();
+        }
+        done(null, user);
+      } catch (error) {
+        done(error, null);
+      }
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL: '/auth/facebook/callback',
+      profileFields: ['id', 'displayName', 'emails'],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ facebookId: profile.id });
+        if (!user) {
+          user = new User({
+            facebookId: profile.id,
             username: profile.displayName,
             email: profile.emails[0].value,
           });
